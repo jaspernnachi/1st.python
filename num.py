e6dc72f431aea25cf564f400cd6e230761c7f552
jaspernnachi <chibuikennachi4@gmail.com>
2024-11-21 11:49:36 +0100

    jasper

---
+++ num.py
@@ -0,0 +1,66 @@
+# # print(int(10/3))
+# # print(complex(10/2))
+# # print(5+0j)
+
+# # import random
+
+# # print(random.randrange(1,20))
+# # print(9/3)
+# # print(9//3)
+# # print(2 ** 5)
+# # print(2*2*2*2*2)
+# # print(10/2)
+# # print(10//2)
+# # print((74+6)/3//2*5)
+# # print(round(4.5))
+
+# # a=10
+# # print(id(a))
+
+# # poem = ''' Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti excepturi ipsum sed. Blanditiis necessitatibus, ducimus quibusdam sed totam ratione quia deserunt nostrum distinctio quaerat ipsam ullam, aperiam facilis ut vero.
+# # '''
+# # print(poem)
+
+# # name="jasper"
+# # print(f"Hello i'm {name}")
+# # print(len(name))
+# # hub='learnfactory'
+# # print(len(hub))
+# # print(hub[0])
+# # print(hub[0:5])
+# # print(hub[-11:-7])
+
+# # x = "banana"
+# # for x in "banana":
+# #     print(x)
+
+# # mylist = ["apple", "banana", "orange"]
+# # print(mylist)
+
+name_of_student = input('Enter your name:')
+print(f'HELLO, {name_of_student}')
+passmark = int(input('Enter your score:'))
+
+if passmark > 50 or passmark == 50:
+    print(f'Hi! {name_of_student}')
+    print('YOU PASSED!')
+else:
+    print(f'Hi {name_of_student}')
+    print('YOU ARE A FAILURE!')
+
+
+
+a, b = 10, 20
+if 'foo' in ['foo', 'bar', 'wat']:
+    print('innit')
+
+    if a==b:
+     print('True')
+
+    print('Done')
+
+    if a < b:
+            print('Yes')
+
+            print('Another one')
+print('End')
